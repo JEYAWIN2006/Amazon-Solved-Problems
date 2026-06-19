2b8e79f7cd80a28b937042bd715500272a1a51e2
JEYAWIN GJ <gjeyawin@gmail.com>
2026-06-19 15:47:32 +0530

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,3 +1,8 @@
+//2. Warehouse Box Arrangement
+//Problem Statement
+//Amazon warehouses receive thousands of products daily. To optimize space, products are packed into large storage boxes. Each box can hold exactly K products. 
+//If some products remain, an extra box must be allocated. Given the total number of products arriving in a shipment and the c’apacity of each box, determine 
+//how many boxes are required.
 
 import java.util.*;
 public class Main1
